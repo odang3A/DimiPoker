a09a1438cdcfc705079b077075dbe2ebf0092bc7
odang3A <odang2923@naver.com>
2020-07-08 11:11:42 +0900

    logSignout2

--- gameSocketIO/index.js
+++ gameSocketIO/index.js
@@ -49,6 +49,7 @@ module.exports = (socket, next) => {
 
     socket.on("SignOut", (data) => {
         const nick = data.nick;
+        console.log(nick);
         playerAuthModel.findOneAndUpdate({ nick }, { playing: 0 }, (err) => {
             if(err) return console.log("signout err");
             console.log(result);
